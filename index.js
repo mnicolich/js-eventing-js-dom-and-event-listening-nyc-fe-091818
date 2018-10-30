62602a09b3fc9cc7b0155126b19c81750c5fa7e9
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-30 22:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // 1. Get the element
+const main = document.querySelector('main')
 // 2, Add on event listener 
+function onClick() {
+  console.log('i was clicked woah')
+}
+
+main.addEventListener('click', '')
