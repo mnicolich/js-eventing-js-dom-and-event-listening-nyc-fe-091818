a07d328bf5f5af3de3f81520ba608dca906001ca
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-30 22:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // 1. Get the element
 const main = document.querySelector('main')
 // 2, Add on event listener 
-function onClick() {
+function onClick(event) {
   alert('i was clicked')
   console.log('i was clicked woah')
 }
 
 main.addEventListener('click', onClick)
-function onKeyDown(){
+function onKeyDown(event){
+  console.log
   
 }
 
